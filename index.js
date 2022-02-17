62b9f169bbca60a14ea514e4f949bc7bfafd2635
Matthew Stead <mattstead6@gmail.com>
2022-02-17 13:35:40 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,100 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+    
+// }
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gift){
+//     for (let i = 0; i < gifts.length; i++){
+//         console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     }
+//         return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+function writeCards(namesArray, eventName){
+    let newArray = [];
+    for (let i = 0; i < namesArray.length; i++){
+    newArray = [...newArray, `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`];
+} return newArray;
+}
+
+function countDown(numb){
+  
+    while (numb >= 0){
+        console.log(numb);
+        numb--;
+    }
+    // console.log(numb);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+   
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+// function writeCards(arrayOfNames, eventName){
+//     const newArray = []
+//     for (let i = 0; i <= arrayOfNames; i++){
+//     `Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`
+    
+//     newArray.push(i);
+//     return newArray;
+    
+// }
+// }
+
+
+// function writeCards(stringNames, eventNames){
+//     for (let i = 0; i < stringNames.length; i++)
+//     return "Thank you, ${stringNames}, for the wonderful surprise gift!",
+// }
+
+
+// for (let i = 0; i < 2; i++)
